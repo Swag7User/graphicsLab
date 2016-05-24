5c2dfec1e12e85aba6c873560fe27a9d6b669902
dev_lab <dev_lab@imac-stud-076.ifi.uzh.ch>
2016-05-24 18:09:24 +0200

    hhh

--- Assignment_5/graphicsLab/project/implementation/RenderProject.cpp
+++ Assignment_5/graphicsLab/project/implementation/RenderProject.cpp
@@ -661,7 +661,6 @@ void RenderProject::updateRenderQueue(const std::string &camera, const double &d
     }
     
     //BLUR    DOING STAFF¨
-    float boostb=1.0;
     if(boostb>1.0){
     bRenderer().getView()->setViewportSize(bRenderer().getView()->getWidth() / 5, bRenderer().getView()->getHeight() / 5);		// reduce viewport size
     defaultFBO = Framebuffer::getCurrentFramebuffer();	// get current fbo to bind it again after drawing the scene
